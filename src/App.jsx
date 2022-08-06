89980bdce22b9455197146a7680c4da79ba38d46
Luis De Aza <1998.09.07deaza@gmail.com>
2022-08-06 23:07:56 +0800

    Proyecto vacio iniciado, tailwindcss instalado.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h2>Proyecto iniciado</h2>
+    </>
+  )
+}
+
+export default App
